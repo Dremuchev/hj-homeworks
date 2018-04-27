f49efa4eafc1d6231d02afb08fd920d767126323
Andrey <34788837+Dremuchev@users.noreply.github.com>
2018-04-27 22:59:59 +0300

    #2-1

---
+++ html-element-collection/drum-machine/index.js
@@ -0,0 +1,11 @@
+'use strict'
+const btnsArray = Array.from(document.getElementsByClassName('drum-kit__drum'));
+const players = document.getElementsByTagName('audio');
+
+btnsArray.forEach((el, index) => {
+    el.onclick = () => {
+        players[index].pause();
+        players[index].currentTime = 0;
+        players[index].play();
+    }
+})
