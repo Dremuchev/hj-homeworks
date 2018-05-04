071db03c2b03aaf18b4c56cfd592e9d8d7c53ff8
Andrey <andrey.dremuchev@gmail.com>
2018-05-04 12:49:44 +0300

    '#1'

---
+++ dom/contact-list/js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+const contacts = JSON.parse(loadContacts());
+const contactList = document.querySelector('.contacts-list');
+let str = '';
+for(let contact of contacts) {
+    str += '<li data-email="' +
+        contact.email +
+        '" data-phone="' +
+        contact.phone +
+        '"><strong>' +
+        contact.name
+        '</strong></li>';
+}
+contactList.innerHTML = str;
