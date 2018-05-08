09a8beec8067fdd844d128f3329f1f29ea21d560
Andrey <andrey.dremuchev@gmail.com>
2018-05-08 17:52:02 +0300

    '#3'

---
+++ xhr/books-catalog/index.js
@@ -0,0 +1,20 @@
+'use strict';
+const xhr = new XMLHttpRequest();
+xhr.addEventListener('load', onLoad);
+xhr.open('GET', 'https://neto-api.herokuapp.com/book/', true);
+xhr.send();
+let str = '';
+function onLoad() {
+    const books = JSON.parse(xhr.responseText);
+    console.log(books);
+    for (let book of books) {
+        str += '<li>' +
+            'data-title="' + book.title +
+            '" data-author="' + book.author.name +
+            '" data-info="' + book.info +
+            '" data-price="' + book.price +
+            '<img src="' + book.cover.small +
+            '"></li>'
+    }
+    document.getElementById('content').innerHTML = str;
+}
