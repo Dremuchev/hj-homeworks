3a3eb8c3d1993f6b143b295d2cebd7758c425eea
Andrey <andrey.dremuchev@gmail.com>
2018-05-10 14:40:06 +0300

    '#2'

--- xhr/tabs/index.js
+++ xhr/tabs/index.js
@@ -1,10 +1,15 @@
 'use strict';
 const buttons =  document.querySelectorAll('nav a');
 const xhr = new XMLHttpRequest();
+function showLoader() {
+    if(!arguments[0].classList.contains('active')) {
+        document.getElementById('preloader').classList.remove('hidden');
+    }
+}
 function showContent(event) {
     event.preventDefault();
     xhr.addEventListener('load', () => document.getElementById('content').innerHTML = xhr.responseText);
-    xhr.addEventListener('loadstart', () => document.getElementById('preloader').classList.remove('hidden'));
+    xhr.addEventListener('loadstart', showLoader(this));
     xhr.addEventListener('loadend', () => document.getElementById('preloader').classList.add('hidden'));
     xhr.open("GET", this.href, true);
     xhr.send();
