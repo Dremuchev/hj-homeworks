54db9dca4bc84a164956ead144d0f0efa0e3c1fe
Andrey <andrey.dremuchev@gmail.com>
2018-05-10 15:02:58 +0300

    '#2 change 1'

--- xhr/tabs/index.js
+++ xhr/tabs/index.js
@@ -1,20 +1,17 @@
 'use strict';
 const buttons =  document.querySelectorAll('nav a');
 const xhr = new XMLHttpRequest();
-function showLoader() {
-    if(!arguments[0].classList.contains('active')) {
-        document.getElementById('preloader').classList.remove('hidden');
-    }
-}
 function showContent(event) {
     event.preventDefault();
-    xhr.addEventListener('load', () => document.getElementById('content').innerHTML = xhr.responseText);
-    xhr.addEventListener('loadstart', showLoader(this));
-    xhr.addEventListener('loadend', () => document.getElementById('preloader').classList.add('hidden'));
-    xhr.open("GET", this.href, true);
-    xhr.send();
-    document.querySelector('.active').classList.remove('active');
-    this.classList.add('active');
+    if (!this.classList.contains('active') || xhr.responseText === "") {
+        xhr.addEventListener('load', () => document.getElementById('content').innerHTML = xhr.responseText);
+        xhr.addEventListener('loadstart', () => document.getElementById('preloader').classList.remove('hidden'));
+        xhr.addEventListener('loadend', () => document.getElementById('preloader').classList.add('hidden'));
+        xhr.open("GET", this.href, true);
+        xhr.send();
+        document.querySelector('.active').classList.remove('active');
+        this.classList.add('active');
+    }
 }
 for (let btn of buttons) {
     btn.addEventListener('click', showContent);
