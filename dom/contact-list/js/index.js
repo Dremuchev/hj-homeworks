d9bab7564b44605d702f9fcd33fd0563aca55839
Andrey <andrey.dremuchev@gmail.com>
2018-05-13 15:35:34 +0300

    '#1'

--- dom/contact-list/js/index.js
+++ dom/contact-list/js/index.js
@@ -3,12 +3,10 @@ const contacts = JSON.parse(loadContacts());
 const contactList = document.querySelector('.contacts-list');
 let str = '';
 for(let contact of contacts) {
-    str += '<li data-email="' +
-        contact.email +
-        '" data-phone="' +
-        contact.phone +
-        '"><strong>' +
-        contact.name
-        '</strong></li>';
+    str += `<li 
+            data-email="${contact.email}" 
+            data-phone="${contact.phone}">
+            <strong>${contact.name}</strong>
+            </li>`;
 }
 contactList.innerHTML = str;
