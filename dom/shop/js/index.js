4446201793101452127b7a51eb4a067b9ec3defe
Andrey <andrey.dremuchev@gmail.com>
2018-05-13 15:41:57 +0300

    '#2'

--- dom/shop/js/index.js
+++ dom/shop/js/index.js
@@ -3,11 +3,13 @@ const buttons = document.getElementsByClassName('add');
 const items = document.querySelectorAll('.add');
 const cartCount = document.getElementById('cart-count');
 const cartTotalPrice = document.getElementById('cart-total-price');
-const priceArray = [];
+let counter = 0;
+let sum = 0;
 function addToCart() {
-    priceArray.push(parseInt(this.getAttribute('data-price')));
-    cartCount.innerHTML = priceArray.length.toString();
-    cartTotalPrice.innerHTML = getPriceFormatted(priceArray.reduce((a, b) => a+b));
+    counter++;
+    sum += parseInt(this.getAttribute('data-price'));
+    cartCount.innerHTML = counter;
+    cartTotalPrice.innerHTML = getPriceFormatted(sum);
 }
 for(let button of buttons) {
     button.addEventListener('click', addToCart)
