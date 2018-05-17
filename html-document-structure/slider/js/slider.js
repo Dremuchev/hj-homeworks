62e0cc475f59cbb79d5f5ca2351e649a65b7c155
Andrey <andrey.dremuchev@gmail.com>
2018-05-17 18:01:29 +0300

    '#1'

--- html-document-structure/slider/js/slider.js
+++ html-document-structure/slider/js/slider.js
@@ -1,85 +1,66 @@
 'use strict';
-let currentSlide = document.querySelectorAll('.slides')[0].firstElementChild;
-currentSlide.classList.add('slide-current');
 const slides = document.querySelector('.slides');
-const buttons = Array.from(document.querySelectorAll('.slider-nav a'));
-const next = buttons.find((el) => el.dataset.action === 'next');
-const last = buttons.find((el) => el.dataset.action === 'last');
-const prev = buttons.find((el) => el.dataset.action === 'prev');
-const first = buttons.find((el) => el.dataset.action === 'first');
-for (let button of buttons) {
-    button.addEventListener('click', moveSlide);
-    toggleButtons();
+let currentSlide = slides.firstElementChild;
+currentSlide.classList.add('slide-current');
+const buttons = document.querySelectorAll('.slider-nav a');
+for(let button of buttons) {
+    button.addEventListener('click', () => moveSlide(button.dataset.action));
 }
+const next = document.querySelector('[data-action = "next"]');
+const last = document.querySelector('[data-action = "last"]');
+const prev = document.querySelector('[data-action = "prev"]');
+const first = document.querySelector('[data-action = "first"]');
+toggleButtons();
 function toggleButtons() {
     currentSlide = document.querySelector('.slide-current');
-    if (currentSlide.nextElementSibling && currentSlide.previousElementSibling) {
-        next.classList.remove('disabled');
-        last.classList.remove('disabled');
-        prev.classList.remove('disabled');
-        first.classList.remove('disabled');
-    }
     if (!currentSlide.nextElementSibling) {
         next.classList.add('disabled');
         last.classList.add('disabled');
         prev.classList.remove('disabled');
         first.classList.remove('disabled');
-    }
-    if (!currentSlide.previousElementSibling) {
+    } else if (!currentSlide.previousElementSibling) {
         prev.classList.add('disabled');
         first.classList.add('disabled');
         next.classList.remove('disabled');
         last.classList.remove('disabled');
+    } else {
+        next.classList.remove('disabled');
+        last.classList.remove('disabled');
+        prev.classList.remove('disabled');
+        first.classList.remove('disabled');
     }
 
 }
-function moveSlide() {
-    if (this.dataset.action === 'next') {
-        nextSlide();
-    }
-    if (this.dataset.action === 'prev') {
-        prevSlide();
-    }
-    if (this.dataset.action === 'last') {
-        lastSlide();
-    }
-    if (this.dataset.action === 'first') {
-        firstSlide();
-    }
-}
-function nextSlide() {
-    currentSlide = document.querySelector('.slide-current');
-    if (currentSlide.nextElementSibling) {
-        currentSlide.classList.remove('slide-current');
-        const nextSlide = currentSlide.nextElementSibling;
-        nextSlide.classList.add('slide-current');
-    }
-    toggleButtons();
-}
-function prevSlide() {
-    currentSlide = document.querySelector('.slide-current');
-    if (currentSlide.previousElementSibling) {
-        currentSlide.classList.remove('slide-current');
-        const nextSlide = currentSlide.previousElementSibling;
-        nextSlide.classList.add('slide-current');
-    }
-    toggleButtons();
-}
-function lastSlide() {
-    currentSlide = document.querySelector('.slide-current');
-    if (currentSlide.nextElementSibling) {
-        currentSlide.classList.remove('slide-current');
-        const nextSlide = slides.lastElementChild;
-        nextSlide.classList.add('slide-current');
-    }
-    toggleButtons();
-}
-function firstSlide() {
-    currentSlide = document.querySelector('.slide-current');
-    if (currentSlide.previousElementSibling) {
-        currentSlide.classList.remove('slide-current');
-        const nextSlide = slides.firstElementChild;
-        nextSlide.classList.add('slide-current');
+function moveSlide(button) {
+    switch (button) {
+        case 'next':
+            if (currentSlide.nextElementSibling) {
+                currentSlide.classList.remove('slide-current');
+                const nextSlide = currentSlide.nextElementSibling;
+                nextSlide.classList.add('slide-current');
+            }
+            break;
+        case 'prev':
+            if (currentSlide.previousElementSibling) {
+                currentSlide.classList.remove('slide-current');
+                const nextSlide = currentSlide.previousElementSibling;
+                nextSlide.classList.add('slide-current');
+            }
+            break;
+        case 'last':
+            if (currentSlide.nextElementSibling) {
+                currentSlide.classList.remove('slide-current');
+                const nextSlide = slides.lastElementChild;
+                nextSlide.classList.add('slide-current');
+            }
+            break;
+        case 'first':
+            if (currentSlide.previousElementSibling) {
+                currentSlide.classList.remove('slide-current');
+                const nextSlide = slides.firstElementChild;
+                nextSlide.classList.add('slide-current');
+            }
+            break;
     }
     toggleButtons();
 }
