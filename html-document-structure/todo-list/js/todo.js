c764ee3aa58a05ca00b22debd1c204795981d313
Andrey <andrey.dremuchev@gmail.com>
2018-05-17 19:01:07 +0300

    '#2'

--- html-document-structure/todo-list/js/todo.js
+++ html-document-structure/todo-list/js/todo.js
@@ -1,8 +1,14 @@
 'use strict';
-const labels = document.querySelectorAll('label');
-for (let label of labels) {
-    label.addEventListener('click', toggleCheckbox);
+const inputs = document.querySelectorAll('input');
+const done = document.querySelector('.done');
+const undone = document.querySelector('.undone');
+for (let input of inputs) {
+    input.addEventListener('click', toggleCheckbox);
 }
 function toggleCheckbox() {
-    console.log(this)
+    if(this.checked) {
+        done.appendChild(this.parentElement);
+    } else {
+        undone.appendChild(this.parentElement);
+    }
 }
