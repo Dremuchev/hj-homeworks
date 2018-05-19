780175ec54452242972c90947dff4e5e127d3acf
Andrey <andrey.dremuchev@gmail.com>
2018-05-19 11:51:33 +0300

    '#3'

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,10 @@
 'use strict';
+const main = document.querySelector('.items-list');
+main.addEventListener('click', eventHandler);
+function eventHandler(event) {
+    if(event.target.classList.contains('add-to-cart')) {
+        const result = items.find((el) => el.title === event.target.dataset.title);
+        addToCart(result);
+        event.preventDefault();
+    }
+}
