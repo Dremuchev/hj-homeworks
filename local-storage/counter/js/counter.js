ba22f1d8ece8c972ef68228562b5da086fd10a5f
Andrey <andrey.dremuchev@gmail.com>
2018-05-21 22:22:26 +0300

    '#1'

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,29 @@
 'use strict';
+let counter= 0;
+const counterInfo = document.getElementById('counter');
+window.addEventListener('load', () => {
+    counterInfo.innerText = localStorage.getItem('counter');
+    counter = parseInt(localStorage.getItem('counter'));
+});
+document.addEventListener('click', changeCounter);
+function changeCounter(event) {
+    switch(event.target.id) {
+        case 'increment':
+            counter++;
+            counterInfo.innerText = counter;
+            localStorage.counter = counter;
+            break;
+        case 'decrement':
+            counter--;
+            counterInfo.innerText = counter;
+            localStorage.counter = counter;
+            break;
+        case 'reset':
+            localStorage.clear();
+            counter = 0;
+            localStorage.counter = counter;
+            counterInfo.innerText = counter;
+            break;
+    }
+}
+
