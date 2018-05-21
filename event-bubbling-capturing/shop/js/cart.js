d156e691d73721c71f8756d8689baddde0429e7c
Andrey <34788837+Dremuchev@users.noreply.github.com>
2018-05-21 12:28:57 +0300

    #3

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -3,7 +3,7 @@ const main = document.querySelector('.items-list');
 main.addEventListener('click', eventHandler);
 function eventHandler(event) {
     if(event.target.classList.contains('add-to-cart')) {
-        const result = items.find((el) => el.title === event.target.dataset.title);
+        const result = { title: event.target.dataset.title, price: event.target.dataset.price };
         addToCart(result);
         event.preventDefault();
     }
