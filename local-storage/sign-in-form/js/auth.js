05d0d02c5474c860b81bb2efbb0ac8bc3df87d16
Andrey <andrey.dremuchev@gmail.com>
2018-05-23 19:55:09 +0300

    '#2'

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,97 +1,87 @@
 'use strict';
 const xhr = new XMLHttpRequest();
-
 const signIn = document.querySelector('.sign-in-htm');
-signIn.querySelector('.check').checked = true;
 signIn.addEventListener('click', loginIn);
-
+const check = document.getElementById('check');
+const outputIn = signIn.querySelector('.error-message');
+check.addEventListener('click', () => {
+    if(checkboxToggle()) {
+        localStorage.clear();
+        outputIn.innerText = '';
+        check.value = 0;
+        checkboxToggle();
+    } else {
+        check.value = 1;
+        checkboxToggle();
+    }
+})
+const inputs = signIn.querySelectorAll('.input');
+for(let input of inputs) {
+    input.addEventListener('change', inputHandler);
+}
+checkboxToggle();
+function inputHandler() {
+    if(parseInt(check.value) === 1) {
+        localStorage[this.name] = this.value;
+    }
+}
+function checkboxToggle() {
+    check.checked = (parseInt(check.value) === 1) ? true : false;
+    if (check.checked) {
+        signIn.email.value = (localStorage.email) ? localStorage.email : '';
+        signIn.password.value = (localStorage.password) ? localStorage.password : '';
+        return true;
+    } else {
+        signIn.email.value = '';
+        signIn.password.value = '';
+        return false;
+    }
+}
 function loginIn(event) {
+    outputIn.innerText = '';
     if (event.target.classList.contains('button')) {
         event.preventDefault();
         xhr.addEventListener('load', (event) => {
-            console.log(loginForm.get('email'), loginForm.get('password'));
-            signIn.querySelector('.error-message').innerText = JSON.parse(xhr.responseText).message;
-            if(parseInt(signIn.querySelector('.check').value) === 0) {
-                signIn.email.value = '';
-                signIn.password.value = '';
-            } else {
-                signIn.email.value = localStorage.email;
-                signIn.password.value = localStorage.password;
-            }
-            });
+            outputIn.innerText = JSON.parse(xhr.responseText).message;
+            checkboxToggle();
+        })
         const loginForm = new FormData(signIn);
-        xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
-        xhr.send(loginForm);
-
-    } else if (event.target.classList.contains('check')) {
-        let isChecked = parseInt(event.target.value);
-        event.target.value = (isChecked === 1) ? 0 : 1;
-        if (isChecked === 1) {
-            localStorage.clear();
-            signIn.querySelector('.error-message').innerText = '';
-            event.target.value = 0;
-            signIn.email.value = '';
-            signIn.password.value = '';
+        console.log(loginForm.get('email') !== '' && loginForm.get('password') !== '');
+        if (loginForm.get('email') !== '' && loginForm.get('password') !== '') {
+            xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
+            xhr.send(loginForm);
         } else {
-            event.target.value = 1;
-            if (signIn.email.value !== '') {
-                signIn.email.value = localStorage.email;
-            }
-            if (signIn.password.value = '') {
-                signIn.password.value = localStorage.password;
-            }
+            outputIn.innerText = 'Заполните поля!'
+            return;
         }
-    } else {
-        event.target.addEventListener('change', (event) => {
-            if(event.currentTarget.value !== '') {
-                if(parseInt(signIn.querySelector('.check').value) === 1) {
-                    localStorage[event.currentTarget.name] = event.currentTarget.value;
-                } else {
-                    localStorage.clear();
-                }
-            } else {
-            localStorage.clear();
-            event.currentTarget.value = '';
-            }
-            console.log(event.currentTarget.value);
-        });
     }
 }
-
 const signUp = document.querySelector('.sign-up-htm');
-const newUser = {};
-var str;
+const outputUP = signUp.querySelector('.error-message');
 signUp.addEventListener('click', loginUp);
-
 function loginUp(event) {
+    outputUP.innerText = '';
     if(event.target.classList.contains('button')) {
         event.preventDefault();
         event.stopPropagation();
         xhr.addEventListener('load', (event) => {
             if (200 <= xhr.status && xhr.status < 300) {
-                signUp.querySelector('.error-message').innerText = JSON.parse(xhr.responseText).message;
+                outputUP.innerText = JSON.parse(xhr.responseText).message;
             } else {
-                signUp.querySelector('.error-message').innerText = 'Произошла ошибка! Повторите процедуру позднее... Приносим свои извенения!';
+                outputUP.innerText = 'Произошла ошибка! Повторите процедуру позднее... Приносим свои извенения!';
                 throw new Error('Ошибка: ' + xhr.status + ' : ' + xhr.statusText);
             }
         });
         const signUpForm = new FormData(signUp);
+        const newUser = {};
         for (const [key, value] of signUpForm) {
                 newUser[key] = value;
         }
-        str = JSON.stringify(newUser);
+        const str = JSON.stringify(newUser);
+        console.log(str);
+        console.log(newUser);
         xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(str);
-    } else {
-        event.target.addEventListener('change', (event) => {
-            if(event.currentTarget.value !== '') {
-                // console.log(event.currentTarget.value);
-                return;
-            } else {
-            console.log('Поле не должно быть пустым!!!');
-        }
-        console.log(event.currentTarget.value);
-        });
     }
 }
