9ae1470e12724ccb563aa56e09a662183686726e
Andrey <andrey.dremuchev@gmail.com>
2018-05-25 19:29:57 +0300

    '#2'

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counter =document.getElementsByClassName('counter')[0];
+const errors = document.getElementsByClassName('errors')[0];
+connection.addEventListener('open', () => {
+    connection.send(JSON.stringify({connections: '', errors: ''}));
+})
+connection.addEventListener('message', event => {
+    const result = JSON.parse(event.data);
+    counter.innerText = result.connections;
+    errors.innerText = result.errors;
+})
+window.addEventListener('beforeunload', () => {
+    connection.close(1000, 'Connection closed!');
+});
