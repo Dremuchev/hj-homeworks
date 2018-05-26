5bf2b1ff2eab003b250af33bd76f9da6a823211d
Andrey <andrey.dremuchev@gmail.com>
2018-05-26 10:58:39 +0300

    '#1'

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,9 +1,9 @@
 'use strict';
-let counter= 0;
+let counter = 0;
 const counterInfo = document.getElementById('counter');
 window.addEventListener('load', () => {
-    counterInfo.innerText = localStorage.getItem('counter');
-    counter = parseInt(localStorage.getItem('counter'));
+    counterInfo.innerText = (!localStorage.getItem('counter')) ? counter : localStorage.getItem('counter');
+    counter = (!localStorage.getItem('counter')) ? 0 : parseInt(localStorage.getItem('counter'));
 });
 document.getElementsByClassName('wrap-btns')[0].addEventListener('click', changeCounter);
 function changeCounter(event) {
