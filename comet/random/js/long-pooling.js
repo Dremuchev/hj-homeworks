51abc4e8cf8ab297833e78b19b5c666a43e8d571
Andrey <andrey.dremuchev@gmail.com>
2018-06-04 20:29:57 +0300

    '#3'

--- comet/random/js/long-pooling.js
+++ comet/random/js/long-pooling.js
@@ -1,1 +1,18 @@
 'use strict';
+window.addEventListener('load', subscribe);
+
+function subscribe() {
+    const longPooling = new XMLHttpRequest();
+
+    longPooling.addEventListener('load', () => {
+        console.log(parseInt(longPooling.responseText));
+        const flipIt = document.querySelector('.long-pooling .flip-it');
+        if (flipIt) {
+            flipIt.classList.remove('flip-it');
+        }
+        document.querySelector('.long-pooling').children[parseInt(longPooling.responseText)].classList.add('flip-it');
+        subscribe();
+    })
+    longPooling.open('GET', 'https://neto-api.herokuapp.com/comet/long-pooling');
+    longPooling.send();
+}
--- comet/random/js/pooling.js
+++ comet/random/js/pooling.js
@@ -1,1 +1,19 @@
 'use strict';
+const pooling = new XMLHttpRequest();
+window.addEventListener('load', poolingReqest);
+function poolingReqest() {
+    setInterval( () => {
+        pooling.open('GET', 'https://neto-api.herokuapp.com/comet/pooling');
+        pooling.send();
+    }, 5000)
+}
+pooling.addEventListener('load', flipPoolingDiv);
+
+function flipPoolingDiv() {
+    console.log(pooling.responseText);
+    const flipIt = document.querySelector('.pooling .flip-it');
+    if (flipIt) {
+        flipIt.classList.remove('flip-it');
+    }
+    document.querySelector('.pooling').children[parseInt(pooling.responseText)].classList.add('flip-it');
+}
