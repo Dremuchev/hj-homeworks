fee4817e32adc8484e086852b4721d5085cb1a5f
Andrey <andrey.dremuchev@gmail.com>
2018-06-04 18:33:24 +0300

    '#2'

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,6 @@
-'use strict';
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+window.editor.addEventListener('update', sendMyCanvas);
+
+function sendMyCanvas(event){
+    canvas.toBlob(snap => ws.send(snap));
+}
