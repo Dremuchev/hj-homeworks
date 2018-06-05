d5ed25ac89745b1a56b824a71b1fcd5353703e57
Andrey <andrey.dremuchev@gmail.com>
2018-06-05 14:47:16 +0300

    '#1'

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -2,6 +2,7 @@
 const main = document.querySelector('.items-list');
 main.addEventListener('click', eventHandler);
 function eventHandler(event) {
+    console.log(event.target)
     if(event.target.classList.contains('add-to-cart')) {
         const result = { title: event.target.dataset.title, price: event.target.dataset.price };
         addToCart(result);
