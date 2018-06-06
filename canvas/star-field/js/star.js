61e5d8b3d539a16bd68599cffee0bc0598ff5017
Andrey <andrey.dremuchev@gmail.com>
2018-06-06 16:33:08 +0300

    '#1'

--- canvas/star-field/js/star.js
+++ canvas/star-field/js/star.js
@@ -1,33 +1,29 @@
 'use strict';
 const canvas = document.getElementsByTagName('canvas')[0];
-canvas.addEventListener('click', generateStar);
 const ctx = canvas.getContext('2d');
 const PI = Math.PI;
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+canvas.addEventListener('click', generateStar);
 
-function getRandomFloat(min, max, toFix) {
-    return ((Math.random() * (max - min)) + min).toFixed(toFix);
-}
+generateStar();
 
 function generateStar() {
+    ctx.beginPath();
     const colors = [ '#ffffff', '#ffe9c4', '#d4fbff'];
-    const randomQuanity = getRandomInt(200, 400);
+    const randomQuanity = parseInt(Math.random() * (400 - 200) + 200);
     ctx.fillStyle = '#000';
-    ctx.rect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    ctx.fill();
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     for (let i = 0; i < randomQuanity; i++) {
-        const radius = getRandomFloat(0, 0.55, 2);
-        const arcPosition = [
-            getRandomFloat(0.55, canvas.clientWidth - 0.55, 1),
-            getRandomFloat(0.55, canvas.clientHeight - 0.55, 1)
+        const rectPoistion = [
+            Math.random() * (canvas.width - 1) + 1,
+            Math.random() * (canvas.height - 1) + 1
         ];
-        ctx.beginPath();
-        ctx.fillStyle = colors[getRandomInt(0, 3)];
-        ctx.globalAlpha = getRandomFloat(0.8, 1, 1);
-        ctx.arc(...arcPosition, radius, 0, 2 * PI);
-        ctx.fill();
+        const size = [
+            Math.random() * (1.1),
+            Math.random() * (1.1)
+        ];
+        ctx.fillStyle = colors[parseInt(Math.random() * (3))];
+        ctx.globalAlpha = Math.random() * (1 - 0.8) + 0.8;
+        ctx.fillRect(...rectPoistion, ...size);
     }
 }
