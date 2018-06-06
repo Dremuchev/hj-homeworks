1329386d767b90e13c7ebd18c81347d329c76a65
Andrey <andrey.dremuchev@gmail.com>
2018-06-06 12:05:42 +0300

    '#2'

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,9 +1,9 @@
 'use strict';
-const xhr = new XMLHttpRequest();
 const signIn = document.querySelector('.sign-in-htm');
 signIn.addEventListener('click', loginIn);
 const check = document.getElementById('check');
 const outputIn = signIn.querySelector('.error-message');
+
 check.addEventListener('click', () => {
     if(checkboxToggle()) {
     localStorage.clear();
@@ -15,16 +15,20 @@ check.addEventListener('click', () => {
     checkboxToggle();
 }
 })
+
 const inputs = signIn.querySelectorAll('.input');
 for(let input of inputs) {
     input.addEventListener('change', inputHandler);
 }
+
 checkboxToggle();
+
 function inputHandler() {
     if(parseInt(check.value) === 1) {
         localStorage[this.name] = this.value;
     }
 }
+
 function checkboxToggle() {
     check.checked = (parseInt(check.value) === 1) ? true : false;
     if (check.checked) {
@@ -37,7 +41,9 @@ function checkboxToggle() {
         return false;
     }
 }
+
 function loginIn(event) {
+    const xhr = new XMLHttpRequest();
     outputIn.innerText = '';
     if (event.target.classList.contains('button')) {
         event.preventDefault();
@@ -48,23 +54,30 @@ function loginIn(event) {
             checkboxToggle();
         })
         const loginForm = new FormData(signIn);
+        const newUser = {};
+        for (const [key, value] of loginForm) {
+            newUser[key] = value;
+        }
         if (loginForm.get('email') !== '' && loginForm.get('password') !== '') {
             xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
-            xhr.send(loginForm);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify(newUser));
         } else {
-            outputIn.innerText = 'Заполните поля!'
+            outputIn.innerText = 'Заполните поля!';
             return;
         }
     }
 }
+
 const signUp = document.querySelector('.sign-up-htm');
 const outputUP = signUp.querySelector('.error-message');
 signUp.addEventListener('click', loginUp);
+
 function loginUp(event) {
+    const xhr = new XMLHttpRequest();
     outputUP.innerText = '';
     if(event.target.classList.contains('button')) {
         event.preventDefault();
-        event.stopPropagation();
         xhr.addEventListener('load', (event) => {
             if (200 <= xhr.status && xhr.status < 300) {
             outputUP.innerText = JSON.parse(xhr.responseText).message;
@@ -78,9 +91,8 @@ function loginUp(event) {
         for (const [key, value] of signUpForm) {
             newUser[key] = value;
         }
-        const str = JSON.stringify(newUser);
         xhr.open('POST', 'https://neto-api.herokuapp.com/signup');
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(str);
+        xhr.send(JSON.stringify(newUser));
     }
 }
