49b4521b12415f0a9734512752fcab5d03c740f9
Andrey <andrey.dremuchev@gmail.com>
2018-06-09 11:53:46 +0300

    '#1'

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,6 +1,16 @@
 'use strict';
 const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
-window.addEventListener('click', event => {
-    connection.send(JSON.stringify({ x: event.clientX, y: event.clientY }));
-    showBubbles(connection);
+
+window.addEventListener('beforeunload', () => {
+    connection.onclose = function () {};
+    connection.close()
+});
+
+showBubbles(connection);
+
+document.addEventListener('click', event => {
+    const cords = {};
+    cords.x = event.pageX;
+    cords.y = event.pageY;
+    connection.send(JSON.stringify(cords));
 });
