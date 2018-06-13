4c8ccc2a7fddbbdd4c5feefbd31733c83bdd201e
Andrey <andrey.dremuchev@gmail.com>
2018-06-13 11:49:47 +0300

    '#3'

--- media/stories/js/recorder.js
+++ media/stories/js/recorder.js
@@ -1,46 +1,72 @@
 'use strict';
 
 if (navigator.mediaDevices === undefined) {
-  navigator.mediaDevices = {};
+    navigator.mediaDevices = {};
 }
 
 if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = function (constraints) {
-    var getUserMedia = navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia;
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+        var getUserMedia = navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
 
-    if (!getUserMedia) {
-      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+        return new Promise((resolve, reject) => {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+        });
     }
-    return new Promise((resolve, reject) => {
-      getUserMedia.call(navigator, constraints, resolve, reject);
-    });
-  }
 }
 
 function createThumbnail(video) {
-  return new Promise((done, fail) => {
-    const preview = document.createElement('video');
-    preview.src = URL.createObjectURL(video);
-    preview.addEventListener('loadeddata', () => preview.currentTime = 2);
-    preview.addEventListener('seeked', () => {
-      const snapshot = document.createElement('canvas');
-      const context = snapshot.getContext('2d');
-      snapshot.width = preview.videoWidth;
-      snapshot.height = preview.videoHeight;
-      context.drawImage(preview, 0, 0);
-      snapshot.toBlob(done);
+    return new Promise((done, fail) => {
+        const preview = document.createElement('video');
+        preview.src = URL.createObjectURL(video);
+        preview.addEventListener('loadeddata', () => preview.currentTime = 2);
+        preview.addEventListener('seeked', () => {
+            const snapshot = document.createElement('canvas');
+            const context = snapshot.getContext('2d');
+            snapshot.width = preview.videoWidth;
+            snapshot.height = preview.videoHeight;
+            context.drawImage(preview, 0, 0);
+            snapshot.toBlob(done);
+        });
     });
-  });
 }
 
-function record(app) {
-  return new Promise((done, fail) => {
-    app.mode = 'preparing';
-    setTimeout(() => {
-      fail('Не удалось записать видео');
-    }, app.limit);
-  });
+function record(app, config = app.config, limit = app.limit) {
+    return new Promise((done, fail) => {
+        app.mode = 'preparing';
+        app.config = config;
+        app.limit = limit;
+        const result = {};
+        navigator.mediaDevices.getUserMedia(app.config)
+        .then((stream) => {
+            app.preview.srcObject = stream;
+            let recorder = new MediaRecorder(stream);
+            let chunks = [];
+            recorder.addEventListener('dataavailable', event => chunks.push(event.data));
+            recorder.addEventListener('stop', event => {
+                const videoData = new Blob(chunks, { 'type' : recorder.mimeType });
+                chunks = null;
+                recorder = stream = null;
+                app.preview.srcObject = null;
+                result.video = videoData;
+                createThumbnail(videoData)
+                .then((res) => {
+                    result.frame = res;
+                    done(result) ;
+                });
+            });
+            setTimeout(() => {
+                app.mode = 'recording';
+                recorder.start();
+                setTimeout(() => {
+                    recorder.stop();
+                }, app.limit);
+            }, 1000);
+        });
+    });
 }
